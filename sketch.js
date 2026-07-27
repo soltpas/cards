7af3d055796945ef8d2fc04b27f163a751f83012
soltpas <sshuku252192@shukutoku.ed.jp>
2026-07-27 08:42:54 +0000

    HSR

--- sketch.js
+++ sketch.js
@@ -1,9 +1,70 @@
 let m = 0;
 let c = 100;
-let sone;
+let si;
+let sii;
+let siii;
+let siv;
+let sv;
+let svi;
+let svii;
+let sviii;
+let six;
+let sx;
+let sxi;
+let sxii;
+let sxiii;
+let qi;
+let qii;
+let qiii;
+let qiv;
+let qv;
+let qvi;
+let qvii;
+let qviii;
+let qix;
+let qx;
+let qxi;
+let qxii;
+let qxiii;
+let hi;
+let hii;
+let hiii;
+let hiv;
+let hv;
+let hvi;
+let hvii;
+let hviii;
+let hix;
+let hx;
+let hxi;
+let hxii;
+let hxiii;
+let di;
+let dii;
+let diii;
+let div;
+let dv;
+let dvi;
+let dvii;
+let dviii;
+let dix;
+let dx;
+let dxi;
+let dxii;
+let dxiii;
+let j;
+let jj;
 
 function preload(){
-    sone = loadImage("sone.png");
+    const names = [
+        "si","sii","siii","siv","sv","svi","svii","sviii","six","sx","sxi","sxii","sxiii",
+        "qi","qii","qiii","qiv","qv","qvi","qvii","qviii","qix","qx","qxi","qxii","qxiii",
+        "hi","hii","hiii","hiv","hv","hvi","hvii","hviii","hix","hx","hxi","hxii","hxiii",
+        "di","dii","diii","div","dv","dvi","dvii","dviii","dix","dx","dxi","dxii","dxiii","j","jj"
+    ];
+    names.forEach(name => {
+        globalThis[name] = loadImage(`${name}.png`);
+    });
 }
 
 function setup(){
